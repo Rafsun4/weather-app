38318cbccdff053d45686312481131734f3519b9
Rafsun4 <saifrafsun@gmail.com>
2020-08-15 20:45:53 +0600

    Done

--- script.js
+++ script.js
@@ -3,18 +3,16 @@ const api = {
     base: 'https://api.openweathermap.org/data/2.5/weather'
 }
 
-const updateUI = data => {
-    document.getElementById('show_city').innerText = data.name;
-    document.getElementById('show_temparature').innerText = data.main.temp;
-    document.getElementById('weather_status').innerText = data.weather[0].main;
-    document.getElementById('icon').setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
-    document.getElementById('city').value = "";
-}
-
 const getWeatherData = city => {
     fetch(`${api.base}?q=${city}&units=metric&appid=${api.key}`)
     .then(response => response.json())
-    .then(data => updateUI(data))
+    .then(data =>{
+        document.getElementById('show_city').innerText = data.name;
+        document.getElementById('show_temparature').innerText = data.main.temp;
+        document.getElementById('weather_status').innerText = data.weather[0].main;
+        document.getElementById('icon').setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+        document.getElementById('city').value = "";
+    })
     .catch(data => document.getElementById('show_city').innerText = "404 Not Found")
 }
 
